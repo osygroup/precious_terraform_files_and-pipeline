c3e51be1c7e1dbf47a56fe229eb5c0e2c6fd928d
Osy Ernesto <ernesto.ekwonye@leatherback.co>
2024-03-04 18:38:59 +0100

    add terraform files and pipeline

--- terraform_aks/variables.tf
+++ terraform_aks/variables.tf
@@ -1,10 +1,10 @@
 terraform {
-  #required_providers {
-    #azurerm = {
-      #source  = "hashicorp/azurerm"
-      #version = "~>3.0"
-    #}
-  #}
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.0"
+    }
+  }
   backend "azurerm" {
       #resource_group_name  = "tfstate"
       #storage_account_name = "<storage_account_name>"
