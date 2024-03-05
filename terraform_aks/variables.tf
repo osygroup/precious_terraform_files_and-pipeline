f95f65ee02d42e2684203a801401b839af5af9a5
Osy Ernesto <ernesto.ekwonye@leatherback.co>
2024-03-05 17:16:41 +0100

    add terraform files and pipeline

--- terraform_aks/variables.tf
+++ terraform_aks/variables.tf
@@ -1,8 +1,13 @@
 terraform {
+
+# Terraform strongly recommend using the required_providers block to set the
+# Azure Provider source and version being used
+# I don't usually set this in the past, so that the latest version of Azure Provider is set automatically
+
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~>3.0"
+      version = "~>3.0.0"
     }
   }
   backend "azurerm" {
@@ -17,26 +22,4 @@ terraform {
 provider "azurerm" {
     features {}
 
-    subscription_id = var.subscription_id
-    client_id = var.client_id
-    client_secret = var.client_secret
-    tenant_id = var.tenant_id
 } 
-
-variable "subscription_id" {
-    description = "Enter Subscription ID for provisioning resources in Azure"
-}
-
-variable "client_id" {
-    description = "Enter Client ID for application created in Azure AD i.e. App Registratrion"
-}
-
-variable "client_secret" {
-    description = "Enter Client secret of application created in Azure AD"
-}
-
-variable "tenant_id" {
-    description = "Enter Tenant ID of Azure AD"
-}
-
-
