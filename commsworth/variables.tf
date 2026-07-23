774d533bd10e80ff775a08354aa3a50ef58e44b8
Osy Ernesto Ekwonye <46828049+osygroup@users.noreply.github.com>
2026-07-23 15:08:38 -0700

    Create variables.tf

---
+++ commsworth/variables.tf
@@ -0,0 +1,24 @@
+#variable "location" {
+#  default = "West Europe"
+#}
+
+#variable "resource_group_name" {
+#  default = "FinSaas-UAT"
+#}
+
+#variable "cluster_name" {
+#  default = "FinSaas-UAT-aks"
+#}
+
+variable "kubernetes_version" {
+  default = "1.34"
+}
+
+variable "tags" {
+  type = map(string)
+
+  default = {
+    Environment = "UAT"
+    ManagedBy   = "Terraform"
+  }
+}
