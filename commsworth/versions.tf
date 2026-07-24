93c52bd3be80b86e8b05b464b2163176f6209ca9
Osy Ernesto Ekwonye <osygroup@gmail.com>
2026-07-24 01:52:17 +0100

    Update tf file

--- commsworth/versions.tf
+++ commsworth/versions.tf
@@ -13,6 +13,9 @@ terraform {
   }
 
   backend "azurerm" {
+      #resource_group_name  = "tfstate"
+      #storage_account_name = "<storage_account_name>"
+      #container_name       = "tfstate"
       key                  = "terraform.tfstate"
   }
 
